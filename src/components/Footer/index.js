1b843c4726f51380f335bea8cddfbd402d3d9b45
Sokol Paja <kolpaja28@gmail.com>
2021-11-17 13:18:51 +0100

    add final touches

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import { QuestionCircleOutlined, QuestionOutlined } from "@ant-design/icons";
 import { FiFacebook, FiInstagram, FiPhoneCall, FiSettings, FiTwitter, FiYoutube } from "react-icons/fi";
 import logo from '../../utils/assets/icons/8-logo.png'
+import { useMediaQuery } from "react-responsive";
+
+import { Menu } from "antd";
+const { SubMenu } = Menu;
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
 const Footer = () => {
+    const isMobile = useMediaQuery({ maxWidth: 375 })
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+
+    const onOpenChange = keys => {
+        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
     return (
         <div className="footer-container">
             <div className="assists">
@@ -20,61 +37,117 @@ const Footer = () => {
                 </div>
             </div>
             <div className="footer">
-                <div className="top">
-                    <div>
-                        <h5>SHOPPING</h5>
+                {
+                    isMobile ?
+                        <Menu
+                            className="footer-menu"
+                            mode="inline"
+                            openKeys={openKeys}
+                            onOpenChange={onOpenChange}
+                        >
+                            <SubMenu className="menu-submenu" key="sub1" title="AIRPORT">
+                                <Menu.Item key="1">Airport Website</Menu.Item>
+                                <Menu.Item key="2">Departures</Menu.Item>
+                                <Menu.Item key="3">Landing</Menu.Item>
+                                <Menu.Item key="4">Parking</Menu.Item>
+                                <Menu.Item key="4">Terminal Maps</Menu.Item>
+                            </SubMenu>
+                            <SubMenu className="menu-submenu" key="sub2" title="SHOPPING">
+                                <Menu.Item key="1">Option 1</Menu.Item>
+                                <Menu.Item key="2">Option 2</Menu.Item>
+                                <Menu.Item key="3">Option 3</Menu.Item>
+                                <Menu.Item key="4">Option 4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu className="menu-submenu" key="sub3" title="SERVICES">
+                                <Menu.Item key="5">Option 5</Menu.Item>
+                                <Menu.Item key="6">Option 6</Menu.Item>
+                            </SubMenu>
+                            <SubMenu className="menu-submenu" key="sub4" title="CHECK YOU ORDER">
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                        :
+                        <div className="top">
+                            <div>
+                                <h5>SHOPPING</h5>
 
-                        <span>How to shop</span>
-                        <span>FAQs</span>
-                        <span>Brands</span>
-                        <span>Customer Care</span>
+                                <span>How to shop</span>
+                                <span>FAQs</span>
+                                <span>Brands</span>
+                                <span>Customer Care</span>
 
-                    </div>
-                    <div>
-                        <h5>SERVICES</h5>
+                            </div>
+                            <div>
+                                <h5>SERVICES</h5>
 
-                        <span>How to shop</span>
-                        <span>FAQs</span>
-                        <span>Customer Care</span>
-                        <span>Brands</span>
-                        <span>Customer Care</span>
+                                <span>How to shop</span>
+                                <span>FAQs</span>
+                                <span>Customer Care</span>
+                                <span>Brands</span>
+                                <span>Customer Care</span>
 
-                    </div>
-                    <div>
-                        <h5>AIRPORT</h5>
+                            </div>
+                            <div>
+                                <h5>AIRPORT</h5>
 
-                        <span>How to shop</span>
-                        <span>FAQs</span>
-                        <span>Customer Care</span>
-                        <span>Brands</span>
-                    </div>
-                    <div>
-                        <h5>CHECK YOUR ORDER</h5>
+                                <span>How to shop</span>
+                                <span>FAQs</span>
+                                <span>Customer Care</span>
+                                <span>Brands</span>
+                            </div>
+                            <div>
+                                <h5>CHECK YOUR ORDER</h5>
 
-                        <span>SHOPPING</span>
-                        <span><a>TRACK ORDERS</a></span>
-                    </div>
-                    <div></div>
-                    <div className="socials">
-                        <div className="social-icons">
-                            <a><FiFacebook /></a>
-                            <a><FiInstagram /></a>
-                            <a><FiYoutube /></a>
-                            <a><FiTwitter /></a>
+                                <span>SHOPPING</span>
+                                <span><a>TRACK ORDERS</a></span>
+                            </div>
+                            <div></div>
+                            <div className="socials">
+                                <div className="social-icons">
+                                    <a><FiFacebook /></a>
+                                    <a><FiInstagram /></a>
+                                    <a><FiYoutube /></a>
+                                    <a><FiTwitter /></a>
+                                </div>
+                                <div className="logo-footer">
+                                    <img src={logo} alt="brand logo" />
+                                </div>
+                            </div>
                         </div>
-                        <div className="logo-footer">
-                            <img src={logo} alt="brand logo" />
+                }
+                {
+                    isMobile ?
+                        <div className="bot-mob">
+                            <div className="bot-privacy">
+                                <span><a>Privacy</a></span>
+                                <span><a>Cookies</a></span>
+                                <span><a>Legal</a></span>
+                                <span><a>Suppliers</a></span>
+                            </div>
+                            <div className="bot-social">
+                                <a><FiFacebook /></a>
+                                <a><FiInstagram /></a>
+                                <a><FiYoutube /></a>
+                                <a><FiTwitter /></a>
+                                <a><img src={logo} alt="brand logo" /></a>
+                            </div>
+                            <div className="bot-terms">
+                                <span>Content Copywrite 2021 Eurostep P.IVA</span>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="bottom">
-                    <span>Content Copywrite 2021 Eurostep P.IVA</span>
-                    <span className="terms">
-                        <span>Privacy Policy</span>
-                        <span>Cookies</span>
-                        <span>Terns & Conditions</span>
-                    </span>
-                </div>
+                        :
+                        <div className="bottom">
+                            <span>Content Copywrite 2021 Eurostep P.IVA</span>
+                            <span className="terms">
+                                <span>Privacy Policy</span>
+                                <span>Cookies</span>
+                                <span>Terms & Conditions</span>
+                            </span>
+                        </div>
+                }
             </div>
         </div>
     )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import BrandLogo from '../BrandLogo';
@@ -8,14 +9,36 @@ import { FaBars } from "react-icons/fa"
 import plane from '../../utils/assets/icons/airplane.png';
 import bag from '../../utils/assets/icons/shopping-bag.png';
 import { SearchOutlined } from '@ant-design/icons';
+import { Drawer } from 'antd';
+
+import { Menu } from "antd";
+const { SubMenu } = Menu;
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
 const Header = ({ isMobile }) => {
+    const [visible, setVisible] = useState(false);
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+
+    const onOpenChange = keys => {
+        const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+        if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            setOpenKeys(keys);
+        } else {
+            setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+        }
+    };
+    const onClose = () => {
+        setVisible(false);
+    };
+    const showDrawer = () => {
+        setVisible(true);
+    };
     return (
         <div className="header">
             <div className="navigate">
                 {isMobile &&
                     <div className="naviage-menu">
-                        <FaBars className="bars" />
+                        <FaBars className="bars" onClick={() => showDrawer()} />
                         <SearchOutlined className="search-icon" />
                     </div>
                 }
@@ -32,6 +55,49 @@ const Header = ({ isMobile }) => {
                     <Language />
                 </>
             }
+            <Drawer
+                placement="left"
+                width="310"
+                onClose={onClose}
+                closable
+                visible={visible}>
+                <Menu
+                    className="header-menu"
+                    mode="inline"
+                    openKeys={openKeys}
+                    onOpenChange={onOpenChange}
+                >
+                    <SubMenu className="menu-submenu" key="sub1" title="SHOPS">
+                        <Menu.Item key="1">Airport Website</Menu.Item>
+                        <Menu.Item key="2">Departures</Menu.Item>
+                        <Menu.Item key="3">Landing</Menu.Item>
+                        <Menu.Item key="4">Parking</Menu.Item>
+                        <Menu.Item key="4">Terminal Maps</Menu.Item>
+                    </SubMenu>
+                    <SubMenu className="menu-submenu" key="sub2" title="BRANDS">
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </SubMenu>
+                    <SubMenu className="menu-submenu" key="sub3" title="PRODUCTS">
+                        <Menu.Item key="5">Option 5</Menu.Item>
+                        <Menu.Item key="6">Option 6</Menu.Item>
+                    </SubMenu>
+                    <SubMenu className="menu-submenu" key="sub4" title="Services">
+                        <Menu.Item key="9">Option 9</Menu.Item>
+                        <Menu.Item key="10">Option 10</Menu.Item>
+                        <Menu.Item key="11">Option 11</Menu.Item>
+                        <Menu.Item key="12">Option 12</Menu.Item>
+                    </SubMenu>
+                </Menu>
+                <div className="mob-line" />
+                <div className="mob-links">
+                    <Language />
+                    <span className="mob-order"> Check your order</span>
+                </div>
+                <div className="mob-line" />
+            </Drawer>
         </div>
     )
 };
